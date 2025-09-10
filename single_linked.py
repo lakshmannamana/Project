42e68f010a786c221977979e67c16c07e89729ce
lakshmannamana <lakshmannamana15@gmail.com>
2025-09-10 17:41:33 +0530

    Create single_linked.py

---
+++ single_linked.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert at the end
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    # Print the linked list
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Usage
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.print_list()
